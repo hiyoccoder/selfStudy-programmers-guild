5bfa5a06fafe2a853d2c0a9399657f9e9947b58c
hiyoccoder <201990519+hiyoccoder@users.noreply.github.com>
2025-04-26 14:54:40 +0900

    デザイン反映：一部のCSSが反映されず

--- sample-next-project/src/app/page.tsx
+++ sample-next-project/src/app/page.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center">
       <Image
+        className="rounded-full"
         src="/profile/profile.jpg"
         alt="プロフィール画像"
         width={200}
         height={200}
       ></Image>
-      <div>
-        <div>hiyoccoder</div>
-        <div>自己紹介</div>
-        <div>
+      <div className="flex flex-col gap-3 ml-10">
+        <div className="text-center text-5xl">T.U</div>
+        <div className="text-left">自己紹介</div>
+        <div className="leading-lg">
           初めまして！
           <br />
           デザイナー2年、コーダー1年半のひよっこです！
@@ -21,9 +22,13 @@ export default function Home() {
           <br />
           よろしくお願いいたします！
           <br />
-          <div>
-            <div>趣味</div>
-            <div>お仕事</div>
+        </div>
+        <div className="flex gap-5">
+          <div className="flex justify-center items-center text-center box-border h-20 w-20 rounded-full shadow-xl bg-yellow-300 hover:bg-yellow-200">
+            趣味
+          </div>
+          <div className="flex justify-center items-center text-center box-border h-20 w-20 rounded-full shadow-xl bg-red-300 hover:bg-red-200">
+            お仕事
           </div>
         </div>
       </div>
